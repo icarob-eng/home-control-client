f8a8a8749a95d7f25c6f7d1b6e78a3b7a02b562a
joao <joaovicif2021@gmail.com>
2022-07-14 22:51:28 -0300

    Correção de bugs e melhoria na interface

--- app/src/main/java/com/example/tests/activities/MainActivity.java
+++ app/src/main/java/com/example/tests/activities/MainActivity.java
@@ -8,15 +8,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.tests.R;
-import com.example.tests.adapter.RecyclerItemClickListener;
 import com.example.tests.SocketClient;
 import com.example.tests.adapter.RelaysAdapter;
 import com.example.tests.dao.RelayDAO;
@@ -32,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView txtOutput;
     private RecyclerView recyclerRelays;
     private RelaysAdapter relaysAdapter;
-    private List<RelayModel> listaRelay = new ArrayList<RelayModel>();
+    private List<RelayModel> listaRelay = new ArrayList<>();
     private RelayDAO relayDAO;
     private static SocketClient socketClient; // static para ser recuperável
 
@@ -45,6 +42,7 @@ public class MainActivity extends AppCompatActivity {
 
         relayDAO = new RelayDAO(this);
         listaRelay = relayDAO.listar();
+        relaysAdapter = new RelaysAdapter(listaRelay);
 
         fieldIP = findViewById(R.id.edit_ip);
         fieldPort = findViewById(R.id.edit_port);
@@ -61,26 +59,7 @@ public class MainActivity extends AppCompatActivity {
 
         btnClear.setOnClickListener(v -> clearTerminal());
 
-        btnAdd.setOnClickListener(v -> {
-            AlertDialog.Builder alert = new AlertDialog.Builder(this);
-            alert.setTitle("Adicionar item");
-            EditText editText = new EditText(this);
-            alert.setView(editText);
-            alert.setPositiveButton("Confirmar", (dialogInterface, i) -> {
-                String nome = editText.getText().toString().trim();
-                if (!nome.isEmpty()) {
-                    RelayModel relay = new RelayModel();
-                    relay.setNome(nome);
-                    relayDAO.salvar(relay);
-
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                    startActivity(intent);
-                    finish();
-                }
-            });
-            alert.create();
-            alert.show();
-        });
+        btnAdd.setOnClickListener(v -> addRelay());
 
         setupRecycler();
         setupSocket();
@@ -134,67 +113,35 @@ public class MainActivity extends AppCompatActivity {
         runOnUiThread(() -> txtOutput.setText(" > "));
     }
 
+    public void addRelay() {
+
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("Adicionar item");
+        EditText editText = new EditText(this);
+        alert.setView(editText);
+
+        alert.setPositiveButton("Confirmar", (dialogInterface, i) -> {
+
+            String nome = editText.getText().toString().trim();
+
+            if (!nome.isEmpty()) {
+
+                RelayModel relay = new RelayModel();
+                relay.setNome(nome);
+                relayDAO.salvar(relay);
+                listaRelay.add(relay);
+                relaysAdapter.notifyItemInserted(listaRelay.size() - 1);
+
+            }
+        });
+
+        alert.create();
+        alert.show();
+    }
     private void setupRecycler() {
 
-        relaysAdapter = new RelaysAdapter(listaRelay);
         recyclerRelays = findViewById(R.id.recycler_relays);
         recyclerRelays.setAdapter(relaysAdapter);
         recyclerRelays.setHasFixedSize(true);
-        recyclerRelays.addOnItemTouchListener(
-                new RecyclerItemClickListener(
-                        getApplicationContext(), recyclerRelays, new RecyclerItemClickListener.OnItemClickListener() {
-                    @Override
-                    public void onItemClick(View view, int position) {
-
-                        AlertDialog.Builder alerta = new AlertDialog.Builder(MainActivity.this);
-                        alerta.setTitle("Atualizar item");
-                        EditText editText = new EditText(MainActivity.this);
-                        editText.setText(listaRelay.get(position).getNome());
-                        alerta.setView(editText);
-
-                        alerta.setPositiveButton("Confirmar", (dialogInterface, i) -> {
-
-                            RelayModel relay = listaRelay.get(position);
-                            String nome = editText.getText().toString().trim();
-
-                            if (!nome.isEmpty()) {
-                                relay.setNome(nome);
-                                relayDAO.atualizar(relay);
-                                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                                startActivity(intent);
-                                finish();
-                            }
-                        });
-
-                        alerta.setNegativeButton("Cancelar", null);
-                        alerta.create();
-                        alerta.show();
-                    }
-
-                    @Override
-                    public void onLongItemClick(View view, int position) {
-                        AlertDialog.Builder alerta = new AlertDialog.Builder(MainActivity.this);
-                        alerta.setTitle("Excluir item");
-                        alerta.setMessage("\nVocê tem certeza que quer excluir o item?");
-
-                        alerta.setPositiveButton("Sim", (dialogInterface, i) -> {
-                            RelayModel relayModel = listaRelay.get(position);
-                            relayDAO.deletar(relayModel);
-                            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                            startActivity(intent);
-                            finish();
-                        });
-
-                        alerta.setNegativeButton("Não", null);
-                        alerta.create();
-                        alerta.show();
-                    }
-
-                    @Override
-                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-
-                    }
-                })
-        );
     }
 }
--- app/src/main/java/com/example/tests/adapter/RelaysAdapter.java
+++ app/src/main/java/com/example/tests/adapter/RelaysAdapter.java
@@ -1,24 +1,26 @@
 package com.example.tests.adapter;
 
+import android.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.tests.R;
+import com.example.tests.dao.RelayDAO;
 import com.example.tests.model.RelayModel;
 
 import java.util.List;
 
 public class RelaysAdapter extends RecyclerView.Adapter<RelaysAdapter.ViewHolderRelay>{
 
-    private List<RelayModel> listaRelay;
+    private final List<RelayModel> listaRelay;
+    private RelayDAO relayDAO;
 
     public RelaysAdapter(List<RelayModel> listaRelay) {
         this.listaRelay = listaRelay;
@@ -35,10 +37,61 @@ public class RelaysAdapter extends RecyclerView.Adapter<RelaysAdapter.ViewHolder
         holder.nome.setText(listaRelay.get(position).getNome());
         holder.id.setText(String.valueOf(position + 1));
 
-        holder.button.setOnClickListener(v -> {
-            new Thread( () -> listaRelay.get(position).turnRelay(position + 1)).start();
+        holder.btnConn.setOnClickListener(v -> new Thread(() -> listaRelay.get(position).turnRelay(position + 1)).start());
+
+        holder.btnEdit.setOnClickListener(v -> {
+
+            AlertDialog.Builder alert = new AlertDialog.Builder(v.getContext());
+            alert.setTitle("Atualizar item");
+            EditText editText = new EditText(v.getContext());
+            editText.setText(holder.nome.getText().toString());
+            alert.setView(editText);
+
+            relayDAO = new RelayDAO(v.getContext());
+
+            alert.setPositiveButton("Confirmar", (dialogInterface, i) -> {
+
+                String nome = editText.getText().toString().trim();
+
+               if (!nome.isEmpty()) {
+
+                   RelayModel relay = listaRelay.get(position);
+                   relay.setNome(nome);
+                   relayDAO.atualizar(relay);
+                   listaRelay.set(position, relay);
+                   notifyItemChanged(position);
+
+               }
+            });
+
+            alert.setNegativeButton("Cancelar", null);
+            alert.create();
+            alert.show();
+
         });
 
+        holder.btnDelete.setOnClickListener(v -> {
+
+           AlertDialog.Builder alert = new AlertDialog.Builder(v.getContext());
+           alert.setTitle("Excluir item");
+           alert.setMessage("Você deseja excluir o item?");
+           relayDAO = new RelayDAO(v.getContext());
+
+           alert.setPositiveButton("Sim", (dialogInterface, i) -> {
+
+               RelayModel relayModel = listaRelay.get(position);
+               relayDAO.deletar(relayModel);
+               listaRelay.remove(position);
+               notifyItemRemoved(position);
+               notifyDataSetChanged();
+
+           });
+
+           alert.setNegativeButton("Não", null);
+           alert.create();
+           alert.show();
+
+        });
     }
 
     @Override
@@ -46,17 +99,18 @@ public class RelaysAdapter extends RecyclerView.Adapter<RelaysAdapter.ViewHolder
         return listaRelay.size();
     }
 
-    public class ViewHolderRelay extends RecyclerView.ViewHolder {
+    public static class ViewHolderRelay extends RecyclerView.ViewHolder {
 
         TextView id, nome;
-        ImageButton button;
+        ImageButton btnConn, btnEdit, btnDelete;
 
         public ViewHolderRelay(@NonNull View itemView) {
             super(itemView);
             nome = itemView.findViewById(R.id.relay_nome);
             id = itemView.findViewById(R.id.relay_id);
-            button = itemView.findViewById(R.id.relay_button);
-
+            btnConn = itemView.findViewById(R.id.relay_button);
+            btnEdit = itemView.findViewById(R.id.btn_editrelay);
+            btnDelete = itemView.findViewById(R.id.btn_delete);
         }
     }
 }
